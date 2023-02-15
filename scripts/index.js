9fe56646aa48be4cd11ec8ab9f9607c0da022e77
Yuliya-Lov <jul-yaris@yandex.ru>
2023-02-15 13:44:26 +0300

    исправить замечания в первой итерации

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,34 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let editForm = document.querySelector('.edit-form');
-let userName = document.querySelector('.edit-form__user-name');
-let userDescription = document.querySelector('.edit-form__user-description');
-let editSubmitButton = document.querySelector('.edit-form__submit');
-let editResetButton = document.querySelector('.edit-form__button-close');
+let userName = document.querySelector('.edit-form__input_value_name');
+let userDescription = document.querySelector('.edit-form__input_value_description');
+let editCloseButton = document.querySelector('.popup__button-close');
 let ProfileName = document.querySelector('.profile__name');
 let ProfileDescription = document.querySelector('.profile__self-description');
 
-userName.setAttribute('value', ProfileName.textContent);
-userDescription.setAttribute('value', ProfileDescription.textContent);
-
-
-editButton.addEventListener('click', openPopup => {
+function openPopup() {
   popup.classList.add('popup_opened');
-})
+  userName.value =  ProfileName.textContent;
+  userDescription.value =  ProfileDescription.textContent;
+}
 
-editResetButton.addEventListener('click', closePopup => {
-  popup.classList.remove('popup_opened');
-})
-
-function saveProfileInfo(evt) {
-  evt.preventDefault();
-  console.log(userName.value);
-  ProfileName.textContent = userName.value;
-  ProfileDescription.textContent = userDescription.value;
+function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-editSubmitButton.addEventListener('click', saveProfileInfo);
+function saveProfileInfo(evt) {
+  evt.preventDefault();
+  ProfileName.textContent = userName.value;
+  ProfileDescription.textContent = userDescription.value;
+  closePopup();
+}
+
+editButton.addEventListener('click', openPopup);
+
+editCloseButton.addEventListener('click', closePopup);
+
+editForm.addEventListener('submit', saveProfileInfo);
 
 
 
